f3a42c24b10a46e9a81a1299a177984b5315b794
Eliezer Marques <neto15.cg@gmail.com>
2021-03-15 19:13:33 -0300

    Altera msg de erro

--- server/handlers/user.go
+++ server/handlers/user.go
@@ -15,7 +15,7 @@ type UserHandler struct {
 func (handler *UserHandler) Get(c echo.Context) error {
 	u, err := handler.get()
 	if err != nil {
-		return errors.Wrap(err, "Fail to list faq")
+		return errors.Wrap(err, "Fail to list user")
 	}
 
 	return c.JSON(http.StatusOK, u)
